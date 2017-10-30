c74bcbb66ef391f1e3479ffa7dea1e5ecb2dbc6b
Amey <amey@llip.life>
2017-10-30 11:32:02 -0700

    delete extra space

--- test/2.js
+++ test/2.js
@@ -1,7 +1,4 @@
-
 const solution = require('../solutions/2.js');
-// solution = require('../yourSolution').solution;
-
 const test = (a, b,c, result) => {
   if(solution(a,b,c) === result) {
     console.log(`Correct`);
@@ -9,7 +6,6 @@ const test = (a, b,c, result) => {
     console.log(`Wrong. Expected:`, result);
   }
 };
-
 test(5,6,7,7);
 test(3,2,6,2);
 test(3,2,5,5);
